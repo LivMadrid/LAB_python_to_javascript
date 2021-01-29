bb3cea6c92067b30d516502ccb097b98849afcc7
Olivia Boynton <liv.madrid14@gmail.com>
2021-01-29 02:16:58 +0000

    Complete functions.js except calculateTotal

--- functions.js
+++ functions.js
@@ -3,14 +3,50 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+    return (town === 'San Francisco');
+}
 
+isHometown('San Francisco')
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    
+    return (firstName + " " + lastName);
+}
 
+getFullName("John", "Doe")
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax) {
+    const subtotal = basePrice * (1 + tax);
+    const fee = 0;
+    
+    
+
+    if (state === "CA") {
+        fee = 0.03 * subtotal;
+
+    }
+
+    else if (state === "PA") {
+        fee = 2;
+    }
+
+    else if (state === "MA") {
+    
+        if (basePrice <= 100) {
+            fee = 1;
+        }
+        else { 
+            fee = 3;
+        }
+    }
+    const total = subtotal + fee;
+    console.log(total);
+    return (subtotal + fee);
+}
+
+calculateTotal(500, "CA", 0.05);
