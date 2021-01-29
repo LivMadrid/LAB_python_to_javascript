f1716f63dcf9b2daa6ccadc09ce8970dfcc7a6be
Olivia Boynton <liv.madrid14@gmail.com>
2021-01-29 01:49:43 +0000

    Intial Commit/ Complete Arrays.js   Co-Authored by Vicki Erjavec <verjavec@gmail.com>

---
+++ arrays.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  for (const i in items)  {
+    console.log(items[i], i);  
+    }
+  }  
+
+
+// printIndices(['apple', 'berry', 'cherry']);
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  const result = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
+  
+}
+
+everyOtherItem(['apple', 'berry', 'cherry']);
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  items.sort((a, b) => a - b);
+  const sliced = items.slice(0,3);
+  const reversed = sliced.reverse();
+
+  console.log('reversed:', reversed);
+}
+
+smallestNItems([1, 30, 4, 21, 10000], 3);
+
+
